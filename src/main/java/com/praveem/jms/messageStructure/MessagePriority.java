4b821b824b32bc1a6034cae67750ee45b4f2ee7e
nitsolution2021 <praveen@nitsolution.in>
2022-01-11 16:57:27 +0530

    Check Priority of each Message

--- src/main/java/com/praveem/jms/messageStructure/MessagePriority.java
+++ src/main/java/com/praveem/jms/messageStructure/MessagePriority.java
@@ -2,12 +2,16 @@ package com.praveem.jms.messageStructure;
 
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.JMSProducer;
+import javax.jms.Message;
 import javax.jms.Queue;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import io.netty.util.internal.shaded.org.jctools.queues.MessagePassingQueue.Consumer;
 
 public class MessagePriority {
 
@@ -15,32 +19,34 @@ public class MessagePriority {
 
 		InitialContext context = new InitialContext();
 		Queue queue = (Queue) context.lookup("queue/myQueue");
-		
-		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
-			JMSContext jmsContext = cf.createContext()){
+
+		try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()) {
 			JMSProducer producer = jmsContext.createProducer();
-			
+
 			String[] messages = new String[3];
-			
+
 			messages[0] = "Message One";
 			messages[1] = "Message Two";
 			messages[2] = "Message Three";
-			
+
 			producer.setPriority(3);
 			producer.send(queue, messages[0]);
-			
+
 			producer.setPriority(1);
 			producer.send(queue, messages[1]);
-			
+
 			producer.setPriority(9);
 			producer.send(queue, messages[2]);
-			
-			
+
 			JMSConsumer consumer = jmsContext.createConsumer(queue);
-			
-			for(int i=0;i<3;i++) {
-				System.out.println(consumer.receiveBody(String.class));
+
+			for (int i = 0; i < 3; i++) {
+				Message receivedMessage = consumer.receive();
+				System.out.println(receivedMessage.getJMSPriority());
 			}
+		} catch (JMSException e) {
+			e.printStackTrace();
 		}
 	}
 
